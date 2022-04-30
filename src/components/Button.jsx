d3716a13a7422826fd4c952aa1cbc905aeeaf60b
Isabel <ilimasc3@gmail.com>
2022-04-29 23:32:30 -0700

    add paws icon to tab, page title, and button

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,18 @@
+import Paw from "../images/paw-print.png"
+import styled from 'styled-components'
+
+const StyledDiv = styled.img`
+    width: 1em;
+    margin-left: 8px;
+    margin-right: 8px;
+`
+
 const Button = ({onClick}) =>{
     return(
         <div>
+            {/* <StyledDiv src={Paw} alt="paw print logo" width="20px"/> */}
             <button onClick={onClick}>New Doggo</button>
+            {/* <StyledDiv src={Paw} alt="paw print logo" width="20px"/> */}
         </div>
     )
 }
--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -1,8 +1,32 @@
+import Paw from "../images/paw-print.png"
+import styled from 'styled-components'
+
+const StyledImage = styled.img`
+    width: 1.5em;
+    `
+const StyledDiv = styled.div`
+    width:100%;
+    `
+const StyledH1 = styled.h1`
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    `
+
+
 const Title = ({name}) => {
     return(
-        <div>
-            <h1>{name}</h1>
-        </div>
+        <StyledDiv>
+            <StyledH1>
+            <StyledImage src={Paw} alt="paw print logo" width="100px"/>
+            <StyledImage src={Paw} alt="paw print logo" width="100px"/>
+            <StyledImage src={Paw} alt="paw print logo" width="100px"/>
+                {name}
+            <StyledImage src={Paw} alt="paw print logo" width="100px"/>
+            <StyledImage src={Paw} alt="paw print logo" width="100px"/>
+            <StyledImage src={Paw} alt="paw print logo" width="100px"/>
+            </StyledH1>
+        </StyledDiv>
     )
 }
 
